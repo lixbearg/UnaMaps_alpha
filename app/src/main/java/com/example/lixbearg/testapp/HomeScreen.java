04d07eae91565d2cb02689bc893be2b4979ce2bb
lixbearg <gabriellibrelon@gmail.com>
2015-10-01 22:32:37 -0300

    Criação TileView

--- app/src/main/java/com/example/lixbearg/testapp/HomeScreen.java
+++ app/src/main/java/com/example/lixbearg/testapp/HomeScreen.java
@@ -8,23 +8,67 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.util.Log;
+import android.view.View;
 import android.widget.ExpandableListView;
+import android.widget.ImageView;
 import android.widget.Toast;
 
-public class HomeScreen extends Activity {
+import com.qozix.tileview.TileView;
+import com.qozix.tileview.markers.MarkerEventListener;
+
+public class HomeScreen extends TileViewActivity {
 
     private final String TAG = "TKT";
     private TouchImageView img;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.home_screen);
+        //setContentView(R.layout.home_screen);
         Log.d(TAG, "Método onCreate");
-        img = (TouchImageView) findViewById(R.id.mapa);
-        img.setMaxZoom(6f);
-        img.setZoom(1.5f);
+//        img = (TouchImageView) findViewById(R.id.mapa);
+//        img.setMaxZoom(6f);
+//        img.setZoom(1.5f);
+
+        //Tileview
+        TileView tileView = getTileView();
+        tileView.setSize(1200, 1051);  // the original size of the untiled image
+        tileView.addDetailLevel(1f, "tile-%row%-%col%.png", "samples/maps_alpha.jpg");
+
+
+        // let's use 0-1 positioning...
+        tileView.defineRelativeBounds(0, 0, 1, 1);
+
+        // center markers along both axes
+        tileView.setMarkerAnchorPoints(-0.5f, -0.5f);
+
+        // add a marker listener
+        tileView.addMarkerEventListener(markerEventListener);
+
+        // add some pins...
+        addPin( 0.213f, 0.478f );
+
+        // scale it down to manageable size
+        tileView.setScale( 0.5 );
+
+        // center the frame
+        frameTo( 0.5, 0.5 );
+
+        setContentView(tileView);
     }
+
+    private void addPin( double x, double y ) {
+        ImageView imageView = new ImageView( this );
+        imageView.setImageResource( R.drawable.pin );
+        getTileView().addMarker( imageView, x, y );
+    }
+
+    private MarkerEventListener markerEventListener = new MarkerEventListener() {
+        @Override
+        public void onMarkerTap( View v, int x, int y ) {
+            Toast.makeText( getApplicationContext(), "You tapped a pin", Toast.LENGTH_LONG ).show();
+        }
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
